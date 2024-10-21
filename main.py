6d87aa009d7a4667602fd26ff2e159f2a916dcbb
Samarth100Coder <kumarsamarth272@gmail.com>
2024-10-21 20:18:41 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+a={5,9,12,65,857,950}
+print('Set a:',a)
+b={1376,83,48,354,87,99,65,65}
+print('Set b:',b)
+c={'a',536,65.87,True}
+print('Set c:',c)
+a.add(84)
+print('Updated Set a:',a)
+a.pop()
+print('Updated Set a:',a)
